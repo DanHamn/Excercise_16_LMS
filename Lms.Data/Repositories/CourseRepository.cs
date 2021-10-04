dd81d569729f2ad5c376e8afa81b1c3330a70166
Daniel H <naduran@live.se>
2021-10-04 15:07:39 +0200

    Repositories

---
+++ Lms.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,57 @@
+﻿using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using Lms.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Repositories
+{
+    class CourseRepository : ICourseRepository
+    {
+        private readonly LmsApiContext db;
+        public CourseRepository(LmsApiContext context)
+        {
+            db = context ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public void Add(Course course)
+        {
+            db.Add(course);
+        }
+
+        public async Task<bool> AnyAsync(int? id)
+        {
+            return await db.Course.AnyAsync(g => g.Id == id);
+        }
+
+        public async Task<Course> FindAsync(int? id)
+        {
+            return await db.Course.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+        {
+            return await db.Course.ToListAsync();
+        }
+
+        public async Task<Course> GetCourseAsync(int? id)
+        {
+            return await db.Course
+                .FirstOrDefaultAsync(e=>e.Id==id);
+        }
+
+        public void Remove(Course course)
+        {
+            db.Course.Remove(course);
+        }
+
+        public void Update(Course course)
+        {
+            db.Course.Update(course);
+        }
+    }
+}
---
+++ Lms.Data/Repositories/ModuleRepository.cs
@@ -0,0 +1,56 @@
+﻿using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using Lms.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Repositories
+{
+    class ModuleRepository : IModuleRepository
+    {
+        private readonly LmsApiContext db;
+        public ModuleRepository(LmsApiContext context)
+        {
+            db = context ?? throw new ArgumentNullException(nameof(db));
+        }
+        public void Add(Module module)
+        {
+            db.Add(module);
+        }
+
+        public async Task<bool> AnyAsync(int? id)
+        {
+            return await db.Module.AnyAsync(g => g.Id == id);
+        }
+
+        public async Task<Module> FindAsync(int? id)
+        {
+            return await db.Module.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Module>> GetAllModulessAsync()
+        {
+            return await db.Module.ToListAsync();
+        }
+
+        public async Task<Module> GetModulesAsync(int? id)
+        {
+            return await db.Module
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public void Remove(Module module)
+        {
+            db.Module.Remove(module);
+        }
+
+        public void Update(Module module)
+        {
+            db.Module.Update(module);
+        }
+    }
+}
